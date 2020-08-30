be455cb0df37598b6b43e97b3fcec2622db47cea
MigueLEsPaul <miguelhinojosa994@gmail.com>
2020-08-30 00:18:02 -0400

    modified:   scripts/imgconvert.py

--- scripts/imgconvert.py
+++ scripts/imgconvert.py
@@ -4,23 +4,34 @@ import os
 from os.path import basename
 from glob import glob
 
-def pngs2mp4(fileexpr,imagesize='640x480'):
+
+def pngs2mp4(fileexpr, imagesize='640x480'):
     """ 
-    input: path(full or not) to files  /path/wrfout_sfc_*
+    input: path to files  /path/wrfout_sfc_*
     output: full path to mp4
     """
     newfilename = fileexpr[:-1]
-    
-    
-    
-    
-    cmd1="""convert -delay 100 {} {}.gif""".format(fileexpr,newfilename)
-    print(cmd1)    
-    os.system(cmd1)
-    cmd2="""ffmpeg -f gif -i {}.gif -c:v libx264 -b 20k -minrate 20k -maxrate 20k -s {} {}.mp4""".format(newfilename,imagesize,newfilename)
+
+    cmd1 = """convert -delay 100 {} {}.gif""".format(fileexpr, newfilename)
+    print(cmd1)
+    # os.system(cmd1)
+    #cmd2 = """/usr/bin/ffmpeg -i {}.gif -c:v libx265 -crf 42 -s {} {}.mp4""".format(
+    #    newfilename, imagesize, newfilename)
+    #cmd2 = """/usr/bin/ffmpeg -y -f image2 -pattern_type glob -framerate 1 -i {}.png  -crf 42.0 -vcodec libx264 -filter:v scale=640:480  -profile baseline  -coder 1 -flags +loop -cmp chroma -partitions +parti4x4+partp8x8+partb8x8 -me_method hex -subq 6 -me_range 16 -g 250 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -b_strategy 1 -threads 0 "{}.mp4" """.format(
+        # fileexpr, newfilename)
+    cmd2 = """/usr/bin/ffmpeg -y -i {}.gif  -crf 42.0 -vcodec libx264 -filter:v scale=640:480   "{}.mp4" """.format(
+        newfilename, newfilename)
+    cmd2 = """cat {}.png|/usr/bin/ffmpeg -y -framerate 1 -i -  -crf 42.0 -vcodec libx264 -filter:v scale=640:480  -coder 1 -flags +loop -cmp chroma -partitions +parti4x4+partp8x8+partb8x8 -me_method hex -subq 6 -me_range 16 -g 250 -keyint_min 25 -sc_threshold 40 -i_qfactor 0.71 -b_strategy 1 -threads 0 "{}.mp4" """.format(
+        fileexpr, newfilename)
+   
+   
     print(cmd2)
     os.system(cmd2)
+    return """{}.mp4""".format(newfilename)
 
 
-if __name__ =="__main__":
-    pngs2mp4("/home/miguel/Projects/cfabots/images/wrfout_2020082806/SFC/RAIN/wrfout_2020082806_d3_rain_sfc_*",imagesize='640x480')
+if __name__ == "__main__":
+    pngs2mp4(
+        "/home/miguel/Projects/cfabots/images/wrfout_2020082800/SFC/RAIN/wrfout_2020082800_d3_rain_sfc_*",
+        imagesize='640x480')
+    pngs2mp4('/home/miguel/Pictures/.webcam/20200827/shot*')
